cee3e3d9ef4e5bb557f591b9e3b9fb8b5a877f45
Kingstar <honeysoft@126.com>
2022-08-19 21:57:58 +0800

    update for V1.17

--- src/main/java/com/automvc/enet/order/entity/Orderhistory.java
+++ src/main/java/com/automvc/enet/order/entity/Orderhistory.java
@@ -1,129 +1,122 @@
-/*
- * Copyright 2016-2019 the original author.All rights reserved.
- * Kingstar(aiteasoft@163.com)
- * The license,see the LICENSE file.
- */
-
 package com.automvc.enet.order.entity;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 /**
- * @author AiTeaSoft.com
- * @since  1.0
- * Create on 2019-04-16 11:48:24
+ * @author Honey
+ * Create on 2022-05-08 00:04:13
  */
 public class Orderhistory implements Serializable {
 
-	private static final long serialVersionUID = 15960497491733462L;
-	
-    private Integer id;
-    private String name;
-    private String orderNo;
-    private String seqNO;
-    private BigDecimal num;
-    private String numFlag;
-    private String type;
-    private String detail;
-    private Timestamp datetime;
-    private String remark;
+	private static final long serialVersionUID = 1596763375102L;
 
-    public Integer getId() {
-	return id;
-    }
+	private Integer id;
+	private String name;
+	private String orderNo;
+	private String seqNo;
+	private BigDecimal num;
+	private String numFlag;
+	private String type;
+	private String detail;
+	private Timestamp datetime;
+	private String remark;
 
-    public void setId(Integer id) {
-	this.id = id;
-    }
-   
-    public String getName() {
-	return name;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
-   
-    public String getOrderNo() {
-	return orderNo;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setOrderNo(String orderNo) {
-	this.orderNo = orderNo;
-    }
-   
-    public String getSeqNO() {
-	return seqNO;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setSeqNO(String seqNO) {
-	this.seqNO = seqNO;
-    }
-   
-    public BigDecimal getNum() {
-	return num;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setNum(BigDecimal num) {
-	this.num = num;
-    }
-   
-    public String getNumFlag() {
-	return numFlag;
-    }
+	public String getOrderNo() {
+		return orderNo;
+	}
 
-    public void setNumFlag(String numFlag) {
-	this.numFlag = numFlag;
-    }
-   
-    public String getType() {
-	return type;
-    }
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
 
-    public void setType(String type) {
-	this.type = type;
-    }
-   
-    public String getDetail() {
-	return detail;
-    }
+	public String getSeqNo() {
+		return seqNo;
+	}
 
-    public void setDetail(String detail) {
-	this.detail = detail;
-    }
-   
-    public Timestamp getDatetime() {
-	return datetime;
-    }
+	public void setSeqNo(String seqNo) {
+		this.seqNo = seqNo;
+	}
 
-    public void setDatetime(Timestamp datetime) {
-	this.datetime = datetime;
-    }
-   
-    public String getRemark() {
-	return remark;
-    }
+	public BigDecimal getNum() {
+		return num;
+	}
 
-    public void setRemark(String remark) {
-	this.remark = remark;
-    }
-   
+	public void setNum(BigDecimal num) {
+		this.num = num;
+	}
+
+	public String getNumFlag() {
+		return numFlag;
+	}
+
+	public void setNumFlag(String numFlag) {
+		this.numFlag = numFlag;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public Timestamp getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(Timestamp datetime) {
+		this.datetime = datetime;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
 
 	 public String toString(){	
-		 StringBuffer str=new StringBuffer();	
-		 str.append("Orderhistory[");
-		 str.append("id=").append(id);	
-		 str.append(",name=").append(name);	
-		 str.append(",orderNo=").append(orderNo);	
-		 str.append(",seqNO=").append(seqNO);	
-		 str.append(",num=").append(num);	
-		 str.append(",numFlag=").append(numFlag);	
-		 str.append(",type=").append(type);	
-		 str.append(",detail=").append(detail);	
-		 str.append(",datetime=").append(datetime);	
-		 str.append(",remark=").append(remark);	
+		 StringBuilder str=new StringBuilder();	
+		 str.append("Orderhistory[");			
+		 str.append("id=").append(id);		 
+		 str.append(",name=").append(name);		 
+		 str.append(",orderNo=").append(orderNo);		 
+		 str.append(",seqNo=").append(seqNo);		 
+		 str.append(",num=").append(num);		 
+		 str.append(",numFlag=").append(numFlag);		 
+		 str.append(",type=").append(type);		 
+		 str.append(",detail=").append(detail);		 
+		 str.append(",datetime=").append(datetime);		 
+		 str.append(",remark=").append(remark);		 
 		 str.append("]");			 
-		 return str.toString();	
-	 }				 
+		 return str.toString();			 
+	 }		 
 }
